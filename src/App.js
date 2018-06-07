24065133ce99ed6f289b779d55b6679aede67cff
unknown <zachsotak@yahoo.com>
2018-06-07 18:13:33 +0100

    Commit from command line

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const app = new Clarifai.App({
+    apiKey: 'c0d0db8413f0430bbdc52256560eb761'
+});
 
 const particlesOptions = {
     particles: {
         number: {
-            value: 121,
+            value: 20,
             density: {
                 enable: true,
                 value_area: 800
             }
         },
         opacity: {
-            value: 0.5,
+            value: 0.4,
             random: false,
             anim:{
                 enable: false,
@@ -27,24 +32,48 @@ const particlesOptions = {
         }
     }
 }
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Particles className='particles'
-              params={particlesOptions}
-          />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-      {/*
+    constructor(){
+        super();
+        this.state = {
+            input: '',
+        }
+    }
 
-      <FaceRecognition /> */}
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
 
-      </div>
-    );
-  }
+    onButtonSubmit = () =>{
+        console.log('click');
+        app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+            function(response) {
+                // do something with response
+            },
+            function(err) {
+                // there was an error
+            }
+        );
+    }
+    render() {
+        return (
+          <div className="App">
+              <Particles className='particles'
+                  params={particlesOptions}
+              />
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange}
+                           onButtonSubmit={this.onButtonSubmit()}/>
+          {/*
+
+          <FaceRecognition /> */}
+
+          </div>
+        );
+    }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css';
 
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
             <p className='f3'>
@@ -11,8 +11,11 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-green'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+                    <button
+                        className='w-30 grow f4 link ph3 pv2 dib white bg-green'
+                        onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
