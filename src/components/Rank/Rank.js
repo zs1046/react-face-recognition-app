87d3e6661ca051f23db5d2df2ff442d83d6f8a0f
unknown <zachsotak@yahoo.com>
2018-06-11 21:42:35 +0100

    connect front end to back end

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
-    return(
+const Rank = ({ name, entries }) => {
+    return (
         <div>
             <div className='white f3'>
-                {'Zach, your current rank is...'}
+                {`${name}, your current entry count is...`}
             </div>
             <div className='white f1'>
-                {'#5'}
+                {entries}
             </div>
         </div>
     );
