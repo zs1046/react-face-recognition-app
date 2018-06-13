ff2d38b201b0ee9136ea45e0b775594ab07cdeb0
unknown <zachsotak@yahoo.com>
2018-06-13 21:37:00 +0100

    connect to server on heroku

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input});
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://evening-bastion-28570.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -96,7 +96,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://evening-bastion-28570.herokuapp.com:3000/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
