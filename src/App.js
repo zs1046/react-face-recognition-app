a3fbe11c5ee2555f737769a588df9be4c877ae13
Zach Sotak <zs1046@txstate.edu>
2018-06-13 18:04:17 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
             .then(response => response.json())
             .then(response => {
                 if (response) {
-                    fetch('https://evening-bastion-28570.herokuapp.com:3000/image', {
+                    fetch('https://evening-bastion-28570.herokuapp.com/image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
