5d4488d2d88bc803deeb42c4c1d714895f3d2e0a
unknown <zachsotak@yahoo.com>
2018-06-13 17:11:19 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -36,23 +36,26 @@ const particlesOptions = {
     }
 }
 
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+
+    }
+}
+
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
 
     loadUser = (data) => {
@@ -105,6 +108,8 @@ class App extends Component {
                         .then(count => {
                             this.setState(Object.assign(this.state.user, { entries: count}))
                         })
+                        .catch(console.log)
+
 
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
@@ -114,7 +119,7 @@ class App extends Component {
 
     onRouteChange = (route) => {
         if (route === 'signout') {
-            this.setState({isSignedIn: false})
+            this.setState(initialState)
         } else if (route === 'home') {
             this.setState({isSignedIn: true})
         }
