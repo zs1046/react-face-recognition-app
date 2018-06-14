e80c1a7ffa83966a042bc831f1b32d5734d502c9
unknown <zachsotak@yahoo.com>
2018-06-14 16:47:35 +0100

    adjust particles

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 const particlesOptions = {
     particles: {
         number: {
-            value: 120,
+            value: 35,
             density: {
                 enable: true,
                 value_area: 800
