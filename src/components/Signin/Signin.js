c0e346de0195e4a09f057d9d760253dd431d1c89
Zach Sotak <zachsotak@yahoo.com>
2018-07-25 23:43:41 -0500

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -38,6 +38,7 @@ class Signin extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return (
+            {/* className stylings from Tachyons package */}
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
