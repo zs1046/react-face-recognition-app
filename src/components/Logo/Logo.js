9e54afa93a30b288b64ecb5734365b8a9535a23f
Zach Sotak <zachsotak@yahoo.com>
2018-07-26 00:12:45 -0500

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import tilt for the logo to tilt when hovered over
 import Tilt from 'react-tilt';
 import brain from './brain.png';
 import './Logo.css';
@@ -6,7 +7,9 @@ import './Logo.css';
 
 const Logo = () => {
     return(
+        {/*Tachyons: margin 4, margin top 0 */}
         <div className='ma4 mt0'>
+        {/* Tilt component copy and pasted from the npm site, replaced default image with our logo*/}
             <Tilt className="Tilt br2 shadow-2" options={{ max : 75 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3">
                     <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
