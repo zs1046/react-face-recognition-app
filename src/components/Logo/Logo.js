ed0aea46102ea86134923195376882a8991e8e5f
Zach Sotak <zachsotak@yahoo.com>
2018-07-26 08:44:27 -0500

    Update Logo.js

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import './Logo.css';
 
 const Logo = () => {
     return(
-        {/*Tachyons: margin 4, margin top 0, br2= border radius 2, shadow-2= shadow like feature */}
+        /*Tachyons: margin 4, margin top 0, br2= border radius 2, shadow-2= shadow like feature */
         <div className='ma4 mt0'>
         {/* Tilt component copy and pasted from the npm site, replaced default image with our logo*/}
             <Tilt className="Tilt br2 shadow-2" options={{ max : 75 }} style={{ height: 150, width: 150 }} >
