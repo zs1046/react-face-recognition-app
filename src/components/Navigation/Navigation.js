12a83b79951ae17d29993d93018fa4e42f11a07f
Zach Sotak <zachsotak@yahoo.com>
2018-07-26 00:03:34 -0500

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+//simple component with no state, pure component
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
         return (
+            {/* move sign-in button to the right */}
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            {/*Tachyons stylings = fontsize, make it a link, dim when clicked on, make it black, underlined, padding of 3, and pointer when hovered over */}
                 <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
             </nav>
         );
