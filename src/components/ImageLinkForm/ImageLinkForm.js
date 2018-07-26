7494f18f100a691a90627639561077c59baaf5aa
Zach Sotak <zachsotak@yahoo.com>
2018-07-26 00:27:18 -0500

    Update ImageLinkForm.js

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-
+// Destructuring
 const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return(
         <div>
             <p className='f3'>
                 {'This Magic Brain will detect faces in your pictures. Give it a try!'}
             </p>
+        {/*Need input and a detect button */}
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
+        {/*w-70 = width is 70%, grow= it will grow when hovered over, ph= padding horizontal, dib+ display inline block*/}
                     <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
                     <button
                         className='w-30 grow f4 link ph3 pv2 dib white bg-green'
