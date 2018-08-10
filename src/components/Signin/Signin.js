7b0b0a7c58e86cf722683243f5b064caf29433a3
Zach Sotak <zs1046@txstate.edu>
2018-08-09 21:36:39 -0500

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -38,10 +38,6 @@ class Signin extends React.Component {
     render() {
         const { onRouteChange } = this.props;
         return (
-<<<<<<< HEAD
-            /* className stylings from Tachyons package */
-=======
->>>>>>> parent of c0e346d... Update Signin.js
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
